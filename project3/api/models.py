0634b7d6b5e494d80bb29f13329842416c80627e
nmcmahan <nmcmahan@csumb.edu>
2021-11-03 17:02:03 -0700

    Basic database setup completed

--- project3/api/models.py
+++ project3/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=200);
+    password = models.CharField(max_length=200);
+    constraints = [
+        models.UniqueConstraint(fields=['username'], name='unique username')
+    ]
+
+
+class Items(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE);
+    url = models.CharField(max_length=200);
+    name = models.CharField(max_length=200);
+    description = models.CharField(max_length=500);
+    likes = models.IntegerField(default=0);
