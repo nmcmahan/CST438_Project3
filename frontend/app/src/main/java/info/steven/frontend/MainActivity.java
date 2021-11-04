43d6152d12bb4140f58a14e7678867e0c9d0bebe
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-11-04 10:39:09 -0700

    Created landing page
    Can't move any further without other views and connection to backend created.

--- frontend/app/src/main/java/info/steven/frontend/MainActivity.java
+++ frontend/app/src/main/java/info/steven/frontend/MainActivity.java
@@ -1,14 +1,99 @@
 package info.steven.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_button = findViewById(R.id.login);
+        TextView signUpLink = findViewById(R.id.signUpLink);
+
+        login_button.setOnClickListener(view -> {
+            String name = username.getText().toString();
+            String pass = password.getText().toString();
+
+            if (name.isEmpty() && pass.isEmpty()) {
+                username.setError("This field cannot be blank");
+                password.setError("This field cannot be blank");
+            }
+
+            if (name.isEmpty()) {
+                username.setError("This field cannot be blank");
+            }
+
+            if (pass.isEmpty()) {
+                password.setError("This field cannot be blank");
+            }
+
+            boolean isValid = validate(name, pass);
+
+            if (isValid) {
+//                loadHomeActivity(view, username.getText().toString());
+            } else {
+                alertDialog();
+                boolean foundUser = findUser(name);
+
+                if (!foundUser) {
+                    username.setError("Your username is incorrect");
+                } else {
+                    password.setError("Your password is incorrect");
+                }
+            }
+        });
+
+//        signUpLink.setOnClickListener(view -> loadSignupActivity(view));
+    }
+
+    static boolean validate(String name, String pass) {
+        return true;
+    }
+
+    static boolean findUser(String name) {
+        return true;
+    }
+
+    private void alertDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("Your username and/or password is incorrect.");
+        dialog.setTitle("Invalid Credentials");
+        dialog.setPositiveButton("OK", (dialog1, which) -> {});
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
+    }
+
+/*
+    private void loadSignupActivity(View view) {
+        Intent intent = SignUpActivity.getIntent(getApplicationContext());
+        startActivity(intent);
+    }
+
+    private void loadHomeActivity(View view, String username) {
+        Intent intent = HomeActivity.getIntent(getApplicationContext());
+        intent.putExtra("CURRENT_USERNAME", username);
+        startActivity(intent);
+    }
+*/
+
+    public static Intent getIntent(Context context){
+        return new Intent(context, MainActivity.class);
     }
 }
