f3fab8e6946d98c90ae65bff4693d679310fd74d
nmcmahan <nmcmahan@csumb.edu>
2021-12-01 22:30:07 -0800

    Fixed missing category field

--- api/models.py
+++ api/models.py
@@ -10,5 +10,6 @@ class Items(models.Model):
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE);
     url = models.CharField(max_length=200);
     name = models.CharField(max_length=200);
+    category = models.CharField(max_length=500);
     description = models.CharField(max_length=500);
     likes = models.IntegerField(default=0);
