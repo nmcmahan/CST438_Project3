313e129411bbffb1604a9d66a64373eaa10e1d1a
nmcmahan <nmcmahan@csumb.edu>
2021-12-04 19:07:01 -0800

    Trying to avoid duplicate error

--- project3/api/models.py
+++ project3/api/models.py
@@ -8,8 +8,8 @@ class Users(models.Model):
 
 class Items(models.Model):
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE);
-    username = models.CharField(max_length=200);
-    img_url = models.CharField(max_length=200);
-    title = models.CharField(max_length=200);
-    category = models.CharField(max_length=200);
+    url = models.CharField(max_length=200);
+    name = models.CharField(max_length=200);
+    category = models.CharField(max_length=200, default='Uncategorized');
     likes = models.IntegerField(default=0);
+    username = models.CharField(max_length=200);
