9d364b12aa1cb2f8bd8ca670cbedd09fd6ed0144
nmcmahan <nmcmahan@csumb.edu>
2021-12-04 18:46:14 -0800

    Added uername to image db table

--- project3/api/models.py
+++ project3/api/models.py
@@ -8,6 +8,7 @@ class Users(models.Model):
 
 class Items(models.Model):
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE);
+    username = models.CharField(max_length=200);
     img_url = models.CharField(max_length=200);
     title = models.CharField(max_length=200);
     category = models.CharField(max_length=500);
